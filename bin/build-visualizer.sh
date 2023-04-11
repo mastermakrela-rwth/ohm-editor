19cc3a272e56e386d861edf3ca0357eb9c3745c2
mastermakrela <mastermakrela@gmail.com>
2023-04-11 17:08:16 +0200

    change grammar to WHILE

--- bin/build-visualizer.sh
+++ bin/build-visualizer.sh
@@ -18,9 +18,9 @@ pushd "$BUILD_DIR"
 echo "Ohm editor version: $(git rev-parse HEAD)" > build-info.txt
 
 # Sync $BUILD_DIR with the assets/ subdirectory of the destination dir.
-read -p "Copy editor to $1 (y/n)? " -n 1 -r
-if [[ $REPLY =~ ^[Yy]$ ]]; then
+# read -p "Copy editor to $1 (y/n)? " -n 1 -r
+# if [[ $REPLY =~ ^[Yy]$ ]]; then
   popd
   rsync -av --delete public/ "$1"
   rsync -av --delete "${BUILD_DIR}/" "$1/assets"
-fi
+# fi
